af724db24c2a175c70a6df5bb8a10e6219bcfefa
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-27 17:16:19 +0300

    task 47 finished

--- task_047/Program.cs
+++ task_047/Program.cs
@@ -3,7 +3,28 @@
 // m = 3, n = 4.
 
 // 0,5 7 -2 -0,2
-
 // 1 -3,3 8 -9,9
-
 // 8 7,8 -7,1 9
+
+// //matrix[i, j] = Math.Round(new Random().NextDouble() + new Random().Next(0, 9), 1);
+void FillArrayWRandomDouble(double[,] matrix, int lineslength, int columnslength)
+{
+    for (int i = 0; i < lineslength; i++)
+    {
+        for (int j = 0; j < columnslength; j++)
+        {
+            matrix[i, j] = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 1);
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter matrix's length - lines");
+int l = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter matrix's length - columns");
+int c = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[l, c];
+
+Console.WriteLine("Your matrix is: ");
+FillArrayWRandomDouble(matrix, l, c);

316d292b399c2949f207f9f618840bda081f8ca4
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-29 21:02:18 +0300

    task 50 finished

--- task_050/Program.cs
+++ task_050/Program.cs
@@ -1,4 +1,23 @@
 ﻿
+
+
+// for (int i = 0; i < r; i++)
+// {
+//     for (int j = 0; j < c; j++)
+//     {
+//         if (i<iR || j<jC)
+//         {
+//             Console.WriteLine("No such element");
+//             break;
+//         }
+//         else if (i == iR && j == jC)
+//         {
+//             Console.WriteLine($"value = {matrix[i, j]} ");
+//         }
+//     }
+// }
+
+
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 // Например, задан массив:
@@ -24,26 +43,7 @@ void FillArrayWRandom(int[,] matrix, int rowslength, int columnslength)
     }
 }
 
-void FindByIndexes(int[,] matrix, int rowslength, int columnslength, int index1, int index2)
-{
-    if (index1 > rowslength | index2 > columnslength)
-    {
-        System.Console.WriteLine("There's no such element");
-    }
-    else
-    {
-        for (int i = 0; i < rowslength; i++)
-        {
-            for (int j = 0; j < columnslength; j++)
-            {
-                if (i == index1 && j == index2)
-                {
-                    Console.WriteLine($"value = {matrix[i, j]} ");
-                }
-            }
-        }
-    }
-}
+
 Console.WriteLine("Enter matrix's length - rows");
 int r = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter matrix's length - columns");
@@ -58,4 +58,25 @@ Console.WriteLine("Enter element's index for rows");
 int iR = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter element's index for columns");
 int jC = Convert.ToInt32(Console.ReadLine());
-FindByIndexes(matrix, r, c, iR, jC);
+
+
+
+if ((r < iR) || (c < jC))
+{
+    Console.WriteLine("No such element");
+}
+else
+{
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+
+            if (i == iR && j == jC)
+            {
+                Console.WriteLine($"value = {matrix[i, j]} ");
+                break;
+            }
+        }
+    }
+}
